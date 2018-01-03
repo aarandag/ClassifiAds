c7f352c1a4d277841cb3aa4fbcf5cc09dabd3081
Cris21395 <Cristian.Gomez2@alu.uclm.es>
2018-01-03 12:08:33 +0100

    RetrieverAgent modified

--- src/domain/RetrieverAgent.java
+++ src/domain/RetrieverAgent.java
@@ -26,118 +26,121 @@ import java.util.ArrayList;
 
 public class RetrieverAgent extends Agent{
 	private Object[] args;
-	
+
 	protected void setup() {
 		/* Get arguments */
 		args = getArguments();
-		
+
 		/* check arguments */
 		if(args != null && args.length == 1) {
 
 			/* Get the url */
 			String url = "http://" + (String) args[0] + "/";
-			
+
 			/* Run behaviour */
 			addBehaviour(new RetrievalBehaviour(url));
 		}
 		else
 			System.out.println("Error. You must type an url as an argument");
 	}
-	
+
 	private class RetrievalBehaviour extends Behaviour{
 		private String url;
 		private boolean end;
 		private MessageTemplate template = null;
-		
+
 		/**
 		 * Constructor of the RetrievalBehaviour class
 		 * @param url
 		 */
 		public RetrievalBehaviour(String url) {
 			this.url = url;
-			
+
 			AID aid = new AID();
 			aid.setLocalName("processor");
-			
+
 			/* Return a template from message that matches with 'Processor' */
 			MessageTemplate senderFilter = MessageTemplate.MatchSender(aid);
-			
+
 			/* Return a template from message that matches with this performative */
 			MessageTemplate informFilter = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-			
+
 			/* Build template */
 			template = MessageTemplate.and(senderFilter, informFilter);
-			
+
 		}
-		
+
 		/**
 		 * Initialization of the variables
 		 */
 		public void onStart() {
 			end = false;
 		}
-		
+
 		/**
 		 * Run the process of the behaviour
 		 */
 		public void action() {
-	        print("Fetching %s...", url);
+			print("Fetching %s...", url);
 
-	        Document doc = null;
+			Document doc = null;
 			try {
 				doc = Jsoup.connect(url).get();
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 			}
-	        Elements links = doc.select("a[href]");
-	        ArrayList<String> attributes = new ArrayList<String>();
-	        
-	        /* Retrieve links */
-	        print("\nLinks: (%d) from <%s>", links.size(), url);
-	        for (Element link : links) {
-	        	String attr = link.attr("abs:href");
-	            attributes.add(attr);
-	        }
-	        
-	        /* Prepare message */
-	        AID aid = new AID();
-	        aid.setLocalName("processor");
-	        
-	        /* Send message if we have found links */
-	        if(links.size() > 0) {
-	        	ACLMessage sendMessage = new ACLMessage(ACLMessage.INFORM);
-	        	sendMessage.setSender(getAID());
-	        	sendMessage.addReceiver(aid);
-	        	try {
-					sendMessage.setContentObject(attributes);
-		        	send(sendMessage);
+			Elements links = doc.select("a[href]");
+			ArrayList<String> attributes = new ArrayList<String>();
+
+			/* Retrieve links */
+			print("\nLinks: (%d) from <%s>", links.size(), url);
+			for (Element link : links) {
+				String attr = link.attr("abs:href");
+				attributes.add(attr);
+			}
+			
+			/* Store the web page and ad links */
+			Storage storage = new Storage(attributes, url);
+
+			/* Prepare message */
+			AID aid = new AID();
+			aid.setLocalName("processor");
+
+			/* Send message if we have found links */
+			if(links.size() > 0) {
+				ACLMessage sendMessage = new ACLMessage(ACLMessage.INFORM);
+				sendMessage.setSender(getAID());
+				sendMessage.addReceiver(aid);
+				try {
+					sendMessage.setContentObject(storage);
+					send(sendMessage);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				};
-				
-	        	/* Block behaviour until message is received */
-	        	blockingReceive(template);
-	        	end = true;
-	        }
+
+				/* Block behaviour until message is received */
+				blockingReceive(template);
+				end = true;
+			}
 		}
-		
+
 		/**
 		 * Print message in a predetermine format
 		 * @param msg
 		 * @param args
 		 */
 		private void print(String msg, Object... args) {
-	        System.out.println(String.format(msg, args));
-	    }
-		
+			System.out.println(String.format(msg, args));
+		}
+
 		/**
 		 * Behaviour finalizes if end is equal to true
 		 */
 		public boolean done() {
 			return end;
 		}
-		
+
 		/**
 		 * Kill the agent
 		 */
@@ -146,7 +149,7 @@ public class RetrieverAgent extends Agent{
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * Free resources from the agent
 	 */
